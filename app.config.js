7d701d6f29c697a2ae542d416b19423950ec034f
James <James.zjgao@gmail.com>
2026-01-10 20:51:47 -0600

    fix: repair npm permissions and react version

--- app.config.js
+++ app.config.js
@@ -19,6 +19,7 @@ export default {
     android: {
       package: 'com.snapreceipt.app',
       versionCode: 1,
+      usesCleartextTraffic: true
       permissions: [
         'CAMERA',
         'READ_EXTERNAL_STORAGE',
